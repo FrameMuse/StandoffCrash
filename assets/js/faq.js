def3b02154bda4c30d2223e52ec0b4f8d6a67e46
Valery Zinchenko <minicablestone@gmail.com>
2020-04-06 01:35:28 +0400

    v1.04

---
+++ assets/js/faq.js
@@ -0,0 +1,12 @@
+// FAQ
+
+var faq_content = $(".faq__content");
+faq_content.each(function () {
+    $(this).css({ "--height": $(this).find(">").outerHeight() + (2).toPx(faq_content) });
+});
+
+$(document).on("click", ".faq__summary", function () {
+    const clause = $(this).parent();
+    $(".faq__clause").not(clause).removeClass("faq__clause--deployed");
+    clause.toggleClass("faq__clause--deployed");
+});
